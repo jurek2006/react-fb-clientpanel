0e5bf7ae674fa9cc3cb1986d21af15bf6632252a
Jurek <jurek2006@gmail.com>
2018-10-23 11:53:20 +0200

    69. Settings Component

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import EditClient from "./components/clients/EditClient";
 import AddClient from "./components/clients/AddClient";
 import ClientDetails from "./components/clients/ClientDetails";
 import Login from "./components/auth/Login";
+import Settings from "./components/settings/Settings";
 
 import { Provider } from "react-redux";
 import store from "./store";
@@ -42,6 +43,11 @@ class App extends Component {
                                     exact
                                     path="/client/edit/:id"
                                     component={UserIsAuthenticated(EditClient)}
+                                />
+                                <Route
+                                    exact
+                                    path="/settings"
+                                    component={UserIsAuthenticated(Settings)}
                                 />
                                 <Route
                                     exact
