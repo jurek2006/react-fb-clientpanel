a3694e7ee4bfdc4d92d782652e3935e47ac7580d
Jurek <jurek2006@gmail.com>
2018-10-23 13:23:12 +0200

    71. Implementing Settings

--- src/components/clients/EditClient.js
+++ src/components/clients/EditClient.js
@@ -42,6 +42,7 @@ export class EditClient extends Component {
 
     render() {
         const { client } = this.props;
+        const { disableBalanceOnEdit } = this.props.settings;
         if (client) {
             return (
                 <div>
@@ -121,6 +122,7 @@ export class EditClient extends Component {
                                         onChange={this.onChange}
                                         defaultValue={client.balance}
                                         ref={this.balanceInput}
+                                        disabled={disableBalanceOnEdit}
                                     />
                                 </div>
 
@@ -141,13 +143,15 @@ export class EditClient extends Component {
 }
 
 EditClient.propTypes = {
-    firestore: PropTypes.object.isRequired
+    firestore: PropTypes.object.isRequired,
+    settings: PropTypes.object.isRequired
 };
 export default compose(
     firestoreConnect(props => [
         { collection: "clients", storeAs: "client", doc: props.match.params.id }
     ]),
-    connect(({ firestore: { ordered } }, props) => ({
-        client: ordered.client && ordered.client[0]
+    connect(({ firestore: { ordered }, settings }, props) => ({
+        client: ordered.client && ordered.client[0],
+        settings: settings
     }))
 )(EditClient);
